9e3f111b217eb46286addc86a16c61f29ebfdbc5
lucas luiz <luiz161820@gmail.com>
2020-10-26 13:45:51 -0300

    atualizações

--- js/JS Exemplos/ajax-utils.js
+++ js/JS Exemplos/ajax-utils.js
@@ -1,35 +1,36 @@
-(function (global) {
+(function (global) {//IIFE (só as variáveis q eu quero saem daqui)
 
   // Set up a namespace for our utility
-  var ajaxUtils = {};
+  var ajaxUtils = {};//Objeto, vou guardar as funções
   
   
-  // Returns an HTTP request object
+  // Returns an HTTP request object (XMLHttpRequest)
   function getRequestObject() {
-    if (global.XMLHttpRequest) {
-      return (new XMLHttpRequest());
-    } 
+    if (global.XMLHttpRequest) {//SE o browser tem esse objeto
+      return (new XMLHttpRequest());//retorna um novo objeto http desse
+    }
+    //SÓ PRA BROWSERS VELHOS
     else if (global.ActiveXObject) {
       // For very old IE browsers (optional)
       return (new ActiveXObject("Microsoft.XMLHTTP"));
     } 
-    else {
+    else {//AJAX NAO SUPORTADO
       global.alert("Ajax is not supported!");
       return(null); 
     }
   }
   
   
-  // Makes an Ajax GET request to 'requestUrl'
-  ajaxUtils.sendGetRequest = 
-    function(requestUrl, responseHandler) {
-      var request = getRequestObject();
-      request.onreadystatechange = 
-        function() { 
-          handleResponse(request, responseHandler); 
+  // Makes an Ajax GET request PARA 'requestUrl'(SERVIDOR)
+  ajaxUtils.sendGetRequest = //ESSA VAI PRA FORA DA IIFE
+    function(requestUrl, responseHandler) {//RESPONSE HANDLER MANUZEIA A RESPOTA DO SERVIDOR
+      var request = getRequestObject();//new XMLHttpRequest();
+      request.onreadystatechange = //XMLHttpRequest.onreadystatechange - VAO SER OS ESTAGIOS DA COMUNICAÇÃO ENTRE SERVER E NAVEGADOR
+        function() { //executada qndo o servidor vem com a resposta PRA VERIFICAR SE PODE MANDAR PRO USER
+          handleResponse(request, responseHandler); //XMLHttpRequest e Manipulador da resposta, PRECISA ESTAR AQUI PQ SE ESTIVER GLOBAL E FOREM FEITAS 2 REQUESTS AO MESMO TEMPO FUDEO, VAI TROCAR O VALOR GLOBALMENTE
         };
-      request.open("GET", requestUrl, true);
-      request.send(null); // for POST only
+      request.open("GET", requestUrl, true);//abre o comando "GET", servidor, truePRA SER ASSINCRONO(se for falso vai ser assíncrono e o browser vai congelar até vir a resposta)
+      request.send(null); // for POST only SE FOSSE POST OS PARAMETRO N IAM FAZER PARTE DA URL, E ENTÃO IRIAM ALI
     };
   
   
@@ -38,9 +39,9 @@
   // and not an error
   function handleResponse(request,
                           responseHandler) {
-    if ((request.readyState == 4) &&
-       (request.status == 200)) {
-      responseHandler(request);
+    if ((request.readyState == 4) &&//TEM Q TER 4 ESTADOS NESSE REQUEST
+       (request.status == 200)) {//TEM Q ESTAR (200) TD OK
+      responseHandler(request);//SE TUDO OK LIBERA o request pra receber a resposta
     }
   }
   
@@ -49,4 +50,4 @@
   global.$ajaxUtils = ajaxUtils;
   
   
-  })(window);
+})(window);
--- js/JS Exemplos/ex_1.js
+++ js/JS Exemplos/ex_1.js
@@ -9,3 +9,28 @@
 })(window);
 
 Lucao.oqtem();
+var x;
+//SE TODO HTML EXECUTADO, EXECUTAR ESSA FUNÇÃO
+document.addEventListener("DOMContentLoaded", function(event)
+{
+  document.querySelector("button").addEventListener("click", function(){
+    $ajaxUtils
+    .sendGetRequest("data/names.txt",function(request){
+      var name = request.responseText;
+      document.getElementById("content").innerHTML = name;
+    });
+    
+    x = Math.floor(Math.random() * 2);
+    if(x==1)
+    {
+      document.getElementById("resposta").innerHTML = "ééééééé";
+    }
+    else{
+      document.getElementById("resposta").innerHTML = "NÃÃÃÃAÃO";
+    }
+
+    setTimeout(function(){ 
+      document.getElementById("resposta").innerHTML = "";
+    }, 1500);
+  });
+});
